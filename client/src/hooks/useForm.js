21008fdf9059f1a79fe1aa320818e61f4ab3a1bc
Panny Lewis <pannylewis@gmail.com>
2020-07-18 14:00:24 -0500

    added useForm.js custom hooks with useLocalStorage, added checkoutForm.test test for the header.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,31 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+export const useForm = (key, initialValues) => {
+  const [values, setValues] = useLocalStorage([key], initialValues);
+
+  const handleChanges = (e) => {
+    // console.log(e.target.value);
+    setValues({
+      ...values,
+      // username: hello
+      [e.target.name]: e.target.value,
+    });
+    console.log(values);
+  };
+  // const handleChanges = e => {
+  //   setUsername(e.target.value);
+  // };
+
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    if (e) e.preventDefault();
+    // setShowSuccessMessage(true);
+  };
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   alert(username);
+  // };
+
+  return [values, handleSubmit, handleChanges];
+};
